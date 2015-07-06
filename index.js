8eae2cce4b917a0a809110ddd41e7092cc51e781
Ahmad Nassri <ahmad@codeinchaos.com>
2015-07-05 22:51:33 -0400

    update dependencies and use debug-log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var debug = require('debug')('pkg-config')
+var debug = require('debug-log')('pkg-config')
 var path = require('path')
 var find = require('find-root')
 var extend = require('xtend')
